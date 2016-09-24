58ed06c09282c5e598074dcb2711260d561abcb7
Matthieu Clin <matthieu.clin@outlook.com>
2016-09-24 19:02:39 +0200

    Explorer endpoint only available on development environment
    Issues with the proper JSON to put in the files, see [here](https://github.com/strongloop/loopback-component-explorer/issues/173)

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,15 @@
 module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+    var message = "Executing in ";
+    if (process.env.NODE_ENV) {
+        message += process.env.NODE_ENV;
+    } else {
+        message += "production";
+    }
+    console.log(message);
+
+    // Install a `/` route that returns server status
+    var router = server.loopback.Router();
+    router.get('/', server.loopback.status());
+    server.use(router);
+
 };
