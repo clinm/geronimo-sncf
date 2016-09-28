343340dee839db4531c3e1b7dc63e60dba2c3337
Matthieu Clin <matthieu.clin@outlook.com>
2016-09-28 10:31:41 +0200

    Process for js files (close to the one for css)

--- script/prestart.sh
+++ script/prestart.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+
+exec_cleancss=./node_modules/clean-css/bin/cleancss
+exec_uglifyjs=./node_modules/uglify-js/bin/uglifyjs
 # creating css directory for first deployement
 mkdir -p ./client/css;
+mkdir -p ./client/js;
 
 # minifying resources
-./node_modules/clean-css/bin/cleancss websrc/main.css -o ./client/css/main-min.css
+$exec_cleancss websrc/css/main.css -o ./client/css/main-min.css
+
+
+$exec_uglifyjs ./websrc/js/line-chart.js -o ./client/js/line-chart-min.js
