0d7fe33246a7335f8f241b9960a40fdacd41f4ae
Matthieu Clin <matthieu.clin@outlook.com>
2016-10-12 09:53:54 +0200

    Adding meta description

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -2,6 +2,7 @@ module.exports = function(app) {
     var router = app.loopback.Router();
     router.get('/', function(req, res) {
         var js = ['/js/bundle.js'];
+        var meta_description = "Etat en temps réel des trains circulant sur le réseau SNCF";
         res.setHeader('Cache-Control', 'public, max-age=86400');
         res.render('home',{js: js});
     });
