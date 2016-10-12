6d929e513d50716c0689a3a429c7b63a133a20cf
Matthieu Clin <matthieu.clin@outlook.com>
2016-10-12 09:22:48 +0200

    Cache on "/" and remove chart.js (useless for now)

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -2,6 +2,7 @@ module.exports = function(app) {
     var router = app.loopback.Router();
     router.get('/', function(req, res) {
         var js = ['/js/bundle.js'];
+        res.setHeader('Cache-Control', 'public, max-age=86400');
         res.render('home',{js: js});
     });
     app.use(router);
