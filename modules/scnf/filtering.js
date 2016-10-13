a82a2462007eb7e865d4702ec2bafd46b3daa0bd
Matthieu Clin <matthieu.clin@outlook.com>
2016-10-13 18:11:45 +0200

    Adding comments

--- modules/scnf/filtering.js
+++ modules/scnf/filtering.js
@@ -2,17 +2,32 @@ module.exports = (function() {
     var _ = require('underscore');
 
     var filtering = {
+        /**
+         *  Say whether date is after the limit or not
+         * @param {string} date     timestamp
+         * @param {string} limit    timestamp
+         * @returns {boolean}   returns date > limit
+         */
         isAfterDate: function(date, limit) {
             var d = parseInt(date);
             var l = parseInt(limit);
 
             return d >= l;
         },
+        /**
+         *  Returns the current day (at 00:00)
+         * @returns {Date} Midnight of the current day
+         */
         getBeginningOfDay: function() {
             var today = new Date();
             today.setHours(0, 0, 0, 0);
             return today;
         },
+        /**
+         *  Keeps only disruptions that are after the 'limit' date
+         * @param {Array} disruptions   List of disruptions
+         * @param {Number} limit        Timestamp limit in the past
+         */
         filterAfterDay: function(disruptions, limit) {
 
             return _.filter(disruptions, function(item) {
